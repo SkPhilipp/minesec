409233c6b3039ef486134b951c4d94712bbd455e
Philipp Gayret <philipp@hileco.com>
2017-06-28 01:44:39 +0200

    Added updated .gitignore and minesec.py

---
+++ minesec.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+# --- bug bountry index ---------------
+# https://www.bugcrowd.com/bug-bounty-list/
+# https://www.vulnerability-lab.com/list-of-bug-bounty-programs.php
+# https://internetbugbounty.org/
+# https://bugcrowd.com/programs
+# https://hackerone.com/bug-bounty-programs
+# https://hackerone.com/directory?query=type%3Ahackerone&sort=published_at%3Adescending&page=1
+# --- password database ---------------
+# pwdb() {
+#     grep -a --include=*.{slq,csv,txt} $1 -R /d/Leaks
+# }
+# --- learning from others ------------
+# https://bugcrowd.com/leaderboard
