53f9c5b1af2342ad395e82b5857aafd42bb8068f
Philipp Gayret <philipp@hileco.com>
2017-06-28 23:05:49 +0200

    Implemented loading

---
+++ README.md
@@ -0,0 +1,4 @@
+# Setup
+
+    echo "export M_HOME=~/.m" >> ~/.bashrc
+    echo "alias m=$M_HOME/minesec.py" >> ~/.bashrc
---
+++ options.py
@@ -0,0 +1,6 @@
+#     all commands:
+#         default conf=${M_HOME}/conf.yml
+#         commands with '--in' have '--stdin' option
+#         commands with '--out' have '--stdout' option
+#         commands with '--in' allow for variable expressions
+#         commands with '--out' allow for variable expressions
